de9d79c4b0279d44b690d8b73d7d2eb122d94452
Thomas Madden <tmadden@mgh.harvard.edu>
2024-05-15 15:31:44 -0400

    Add allocators

---
+++ src/astroid/allocators.hpp
@@ -0,0 +1,62 @@
+#ifndef ASTROID_ALLOCATORS_HPP
+#define ASTROID_ALLOCATORS_HPP
+
+#include <astroid/common.hpp>
+
+namespace astroid {
+
+template<class T>
+struct allocator_interface
+{
+    // Allocate a cradle::data_owner with sufficient size and alignment to hold
+    // `n` elements of type `T`.
+    virtual std::shared_ptr<cradle::data_owner>
+    allocate(size_t n) = 0;
+};
+
+template<class T>
+struct unique_ptr_data_owner : public cradle::data_owner
+{
+    std::unique_ptr<T> ptr;
+};
+
+template<class T>
+std::shared_ptr<cradle::data_owner>
+make_unique_ptr_owner(std::unique_ptr<T> ptr)
+{
+    unique_ptr_data_owner<T> owner;
+    owner.ptr = std::move(ptr);
+    return std::make_shared<unique_ptr_data_owner<T>>(std::move(owner));
+}
+
+template<class T>
+struct standard_allocator : allocator_interface<T>
+{
+    std::shared_ptr<cradle::data_owner>
+    allocate(size_t n) override
+    {
+        return make_unique_ptr_owner<T[]>(std::make_unique<T[]>(n));
+    }
+};
+
+template<class T>
+allocator_interface<T>&
+default_allocator()
+{
+    static standard_allocator<T> the_allocator;
+    return the_allocator;
+}
+
+struct standard_allocation_policy
+{
+    template<class T>
+    standard_allocator<T>
+    make_allocator()
+    {
+        return standard_allocator<T>();
+    }
+};
+
+} // namespace astroid
+
+#endif
---
+++ src/astroid/shared_memory.hpp
@@ -0,0 +1,41 @@
+#ifndef ASTROID_SHARED_MEMORY_HPP
+#define ASTROID_SHARED_MEMORY_HPP
+
+#include <astroid/allocators.hpp>
+
+#include <cradle/inner/requests/generic.h>
+
+namespace astroid {
+
+template<class T>
+struct shared_memory_allocator : allocator_interface<T>
+{
+    cradle::local_context_intf* context;
+
+    std::shared_ptr<cradle::data_owner>
+    allocate(size_t n) override
+    {
+        // Allocate a shared memory block and assign it.
+        // Note that owner->data() is going to point to the beginning of a
+        // memory-mapped allocation, which should be page-aligned, so there
+        // shouldn't be any issues with alignment here.
+        auto owner = context->make_data_owner(sizeof(T) * n, true);
+        // Invoke the placement new operator to construct the elements.
+        (void) new (owner->data()) T[n];
+        return owner;
+    }
+};
+
+struct shared_memory_allocation_policy
+{
+    cradle::local_context_intf* context;
+
+    template<class T>
+    shared_memory_allocator<T>
+    make_allocator()
+    {
+        return shared_memory_allocator<T>{context};
+    }
+};
+
+} // namespace astroid
