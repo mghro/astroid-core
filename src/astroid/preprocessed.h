67dbe175fceb7c9f19ce172e6b310e38c6e14eda
Thomas Madden <tmadden@mgh.harvard.edu>
2024-05-16 14:25:06 -0400

    Fix preprocessing

--- src/astroid/preprocessed.h
+++ src/astroid/preprocessed.h
@@ -1,7 +1,8 @@
 #ifndef ASTROID_PREPROCESSED_HPP
 #define ASTROID_PREPROCESSED_HPP
 
-#include <cradle/inner/requests/normalization_uuid.h>
 #include <cradle/typing/core/preprocessed.h>
+
+#include <cradle/inner/requests/normalization_uuid.h>
 
 #endif
